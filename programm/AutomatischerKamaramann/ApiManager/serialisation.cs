1fbfdc49128dca9b74d685d74d167bcfe158a44d
Abdelrahman Elsharkawi <s0577909@htw-berlin.de>
2021-12-08 00:10:23 +0100

    minor edit

--- programm/AutomatischerKamaramann/ApiManager/serialisation.cs
+++ programm/AutomatischerKamaramann/ApiManager/serialisation.cs
@@ -13,40 +13,34 @@ namespace ApiManager
 {
     class serialisation
     {
-        public class jsonDeserializer
-        {
-            string Path = $"{AppDomain.CurrentDomain.BaseDirectory}/image.png";
 
-            //Konvertieren von EmguImage zu System.Drawing.Image
-            public void emguToImage(Image<Rgb, Byte> myEmguImage)
-            {
-                //damit kein konflikt 
-                if (File.Exists(Path))
-                {
-                    File.Delete(Path);
-                }
+        string Path = $"{AppDomain.CurrentDomain.BaseDirectory}/image.png";
 
                 Bitmap bmp = myEmguImage.ToBitmap();
                 bmp.Save(Path, ImageFormat.Png);
             }
 
-            public string ImageEncode(Image<Rgb, Byte> myEmguImage)
-            {
-                emguToImage(myEmguImage);
-                //einlesen von dem PNG
-                using (Image myImage = Image.FromFile(Path))
-                {
-                    using (MemoryStream m = new MemoryStream())
-                    {
-                        myImage.Save(m, myImage.RawFormat);
-                        byte[] imageBytes = m.ToArray();
+            Bitmap bmp = myEmguImage.ToBitmap();
+            bmp.Save(Path, ImageFormat.Png);
+        }
 
-                        // Convert byte[] to Base64 String
-                        string base64String = Convert.ToBase64String(imageBytes);
-                        return base64String;
-                    }
+        public string ImageEncode(Image<Rgb, Byte> myEmguImage)
+        {
+            emguToImage(myEmguImage);
+            //einlesen von dem PNG
+            using (Image myImage = Image.FromFile(Path))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    myImage.Save(m, myImage.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
                 }
             }
         }
     }
+
 }
