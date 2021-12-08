176a0808d3f6025db6393d631d2f90594eed0ab1
Feres Touati <Feres.Touati@htwb.onmicrosoft.com>
2021-12-08 17:15:20 +0100

    Minor changes to InFrames class

--- programm/AutomatischerKamaramann/VideoToFrames/inFrames.cs
+++ programm/AutomatischerKamaramann/VideoToFrames/inFrames.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -7,13 +8,30 @@ namespace VideoToFrames
 {
     public class inFrames
     {
-        public Image<Rgb, Byte> currentFrame = null;
-        private Mat Matrix = null;
-
-        public Image<Rgb, Byte> nextFrame(Capture videCapture)
+        
+        public List<Image<Rgb, Byte>> vidToFrames(string Filename)
         {
-            return currentFrame;
+            List<Image<Rgb, Byte>> listFrames = new List<Image<Rgb, Byte>>();
 
+            VideoCapture _capture = new VideoCapture(Filename);
+            bool ProCes = true;
+
+            while (ProCes)
+            {
+                Image<Rgb,Byte> frame = _capture.QueryFrame().ToImage<Rgb,Byte>();
+
+                if (frame != null)
+                {
+                    listFrames.Add(frame);
+                }
+                else
+                {
+                    ProCes = false;
+                }
+            }
+
+            return listFrames;
         }
     }
+    
 }
