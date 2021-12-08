28d557e4952f618d2825b7904107426dc01fbef0
Feres Touati <Feres.Touati@htwb.onmicrosoft.com>
2021-12-08 16:40:40 +0100

    08.12.21_Erste Änderungen

---
+++ programm/AutomatischerKamaramann/Croping/photoCroping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using CommonInterfaces;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Croping
+{
+    public class photoCroping:IFaceDetection,IPoseEstimating
+    {
+        private Image<Rgb, Byte> Cutout = null;
+
+        public Image<Rgb, Byte> Crop(Image<Rgb, Byte> currentFrame, Rectangle[] Object)
+        {
+            return Cutout;
+        }
+        public Image<Bgr, Byte> export(string fileName)
+        {
+            return null;
+        }
+
+        public Rectangle[] faceDet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle[] Poseframing()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ programm/AutomatischerKamaramann/FaceDetection/faceDetiction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetection
+{
+    public class faceDetection
+    {
+        public CascadeClassifier faceCascadeClassifier { set; get; }
+
+        private Mat grayImage = null;
+
+        public faceDetection(CascadeClassifier classifier)
+        {
+            faceCascadeClassifier = classifier;
+        }
+
+        public Rectangle[] FaceDet(List<Image<Rgb, Byte>>currentFrame)
+        {
+
+            return null;
+        }
+    }
+}
