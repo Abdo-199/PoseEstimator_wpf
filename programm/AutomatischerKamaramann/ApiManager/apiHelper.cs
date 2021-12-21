da81f2647edb1cff787425d47a4a238ddea86f98
Abdelrahman Elsharkawi <s0577909@htw-berlin.de>
2021-12-22 00:45:47 +0100

    API Request and Response

--- programm/AutomatischerKamaramann/ApiManager/apiHelper.cs
+++ programm/AutomatischerKamaramann/ApiManager/apiHelper.cs
@@ -7,20 +7,44 @@ using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace ApiManager
 {
     public class apiHelper
     {
-
-        RestClient client = new RestClient("http://141.45.150.62:4713/openapi.json");
-        RestResponse restResponse = new RestResponse();
-        RestRequest request = new RestRequest();
+        public static serialisation DZ = new serialisation();
         public List<Dictionary<string, PointF>> getCoordinates(Image<Rgb,Byte> img)
         {
-           
-            return null;
+            #region ApiCall
+            //Encoding the Image to base64str png
+            string base64string = DZ.ImageEncode(img);
+            //request
+            RestClient client = new RestClient("http://141.45.150.62:4713/predict");
+            RestRequest request = new RestRequest(Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", " application/json");
+            request.AddJsonBody(new { base64str = base64string });
+            #endregion
+            //response
+            IRestResponse response = client.Execute(request);
+            //json string
+            string content = response.Content;
+            //deserializing the JSON string into List<Dictionary<string,List<double>>>
+            var deContent = JsonConvert.DeserializeObject<List<Dictionary<string, List<double>>>>(content);
+            //new list with PointF instead of List<double>
+            List<Dictionary<string, PointF>> coordinats = new List<Dictionary<string, PointF>>();
+            foreach (var person in deContent)
+            {
+                Dictionary<string, PointF> parts = new Dictionary<string, PointF>();
+                foreach (var part in person)
+                {
+                    parts.Add(part.Key, ToPointF(part.Value));
+                }
+                coordinats.Add(parts);
+            }
+            return coordinats;
         }
 
         public  PointF ToPointF(List<double> part)
