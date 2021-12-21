3fb1307586b4a7ad46bdf9e71143f376a3d4a00b
Abdelrahman Elsharkawi <s0577909@htw-berlin.de>
2021-12-22 00:53:10 +0100

    Pose framing

--- programm/AutomatischerKamaramann/PoseEstimating/poseEstimation.cs
+++ programm/AutomatischerKamaramann/PoseEstimating/poseEstimation.cs
@@ -15,7 +15,36 @@ namespace PoseEstimating
         }
         public List<Rectangle> PoseFraming(List<Dictionary<string, PointF>> coordinates)
         {
-            return null;
+            List<Rectangle> Persons = new List<Rectangle>();
+            foreach (var Person in coordinates)
+            {
+                float minX = Person["nose"].X;
+                float maxX = 0;
+                float minY = Person["nose"].Y;
+                float maxY = 0;
+                foreach (var part in Person)
+                {
+                    if (part.Value.X < minX)
+                    {
+                        minX = part.Value.X;
+                    }
+                    if (part.Value.X > maxX)
+                    {
+                        maxX = part.Value.X;
+                    }
+                    if (part.Value.Y < minY)
+                    {
+                        minY = part.Value.Y;
+                    }
+                    if (part.Value.Y > minY)
+                    {
+                        maxY = part.Value.Y;
+                    }
+
+                }
+                Persons.Add(new Rectangle((int)Math.Round(minX), (int)Math.Round(maxY), (int)Math.Round(maxX - minX), (int)Math.Round(maxY - minY)));
+            }
+            return Persons;
         }
 
         public void getPoseestimation()
