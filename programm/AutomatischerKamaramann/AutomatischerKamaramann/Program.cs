397ffe099605375a3c0f7304992ac9707cfd56e5
Feres Touati <Feres.Touati@htwb.onmicrosoft.com>
2022-01-04 20:23:48 +0100

    Minor Fix

--- programm/AutomatischerKamaramann/AutomatischerKamaramann/Program.cs
+++ programm/AutomatischerKamaramann/AutomatischerKamaramann/Program.cs
@@ -17,6 +17,7 @@ namespace AutomatischerKamaramann
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMainMenu());
            
         }
     }
